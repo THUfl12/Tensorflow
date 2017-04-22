bce6174e0468813425368c67b5c23f0b0fb5859c
LiangFeng <1027555983@qq.com>
2017-04-22 10:45:34 +0800

    Update variable_test.py

--- varibale/variable_test.py
+++ varibale/variable_test.py
@@ -3,13 +3,16 @@ import tensorflow as tf
 weights = tf.Variable(tf.random_normal(shape = [1], stddev = 0.35), name = 'weights')
 biases = tf.Variable(tf.zeros([200]), name = 'biases')
 w2 = tf.Variable(tf.random_normal(shape = [1], stddev = 0.35), name = 'w2')
-# w2.assign(weights)
+# before init, assign the values of weights to w2
+w2 = w2.assign(weights)
 
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
+    # init all variables
     print(sess.run(weights))
     print(sess.run(w2))
+    # after init, assign the values of weights to w2
     w2 = w2.assign(weights)
     print(sess.run(w2))
