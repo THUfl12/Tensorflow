16bc1b4e2d171e09936623089ee1cb4c7844ffca
LiangFeng <1027555983@qq.com>
2017-08-01 20:58:13 +0800

    Update ps.py

--- Keras/Simple_Distributed_Test/ps.py
+++ Keras/Simple_Distributed_Test/ps.py
@@ -1,3 +1,2 @@
 import os
-os.system("tensor_python dist_keras.py --job_name=ps --ps_hosts=172.22.191.46:2225 
-          --worker_hosts=172.22.191.46:2226 --task_index=0")
+os.system("tensor_python dist_keras.py --job_name=ps --ps_hosts=172.22.191.46:2225 --worker_hosts=172.22.191.46:2226 --task_index=0")
