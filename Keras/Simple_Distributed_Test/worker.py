7b1836f98a45149d4b5ff970da27d966206f99d0
LiangFeng <1027555983@qq.com>
2017-08-04 13:56:06 +0800

    Update worker.py

--- Keras/Simple_Distributed_Test/worker.py
+++ Keras/Simple_Distributed_Test/worker.py
@@ -1,2 +1,2 @@
 import os
-os.system("tensor_python dist_keras.py --job_name=worker --ps_hosts=172.22.191.46:2225 --worker_hosts=172.22.191.46:2226，172.22.191.45 --task_index=0")
+os.system("tensor_python dist_keras.py --job_name=worker --ps_hosts=172.22.191.46:2225 --worker_hosts=172.22.191.46:2226，172.22.191.45:2225 --task_index=0")
